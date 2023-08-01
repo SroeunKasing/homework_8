17387127d7b3f6599541fbb66c3957d6cf188e45
Chanveasna-ENG <137870404+Chanveasna-ENG@users.noreply.github.com>
2023-08-01 18:10:42 +0700

    Update 5

--- exercise_5.py
+++ exercise_5.py
@@ -3,23 +3,34 @@ def reverse_ascending(numbers):
     ascending_sequence = []
 
     for index, current_value in enumerate(numbers):
+        # we don't want to compare between the first and the last
         if index - 1 < 0:
             continue
 
+        # main operation check for ascending
         if current_value > numbers[index - 1]:
             if ascending_sequence:
+                # ascending has happened before
                 ascending_sequence.append(current_value)
             else:
+                # start noticing ascending
                 ascending_sequence.extend([numbers[index - 1], current_value])
+
+        # ascending has stopped
         else:
+            # when noticing ascending has stopped
             if ascending_sequence:
                 result.extend(ascending_sequence[::-1])
                 ascending_sequence.clear()
+            # ascending still has not started
             else:
                 result.append(numbers[index - 1])
 
+    # if ascending has never stopped
     if ascending_sequence:
         result.extend(ascending_sequence[::-1])
+
+    # if ascending has never started
     if len(result) != len(numbers):
         result.append(numbers[-1])
 
